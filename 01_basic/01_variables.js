67b7d78375ca869a6c5a9ff51b6ae3a12375212b
Umang-B-Anam <umanganam18@gmail.com>
2025-08-26 09:50:06 +0530

    first commit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123;
+let accountEmail="umanganam18@gmail.com"
+var accountPassword="1234"
+accountCity="Mandvi"
+let variable;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,variable]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+/* 
+number
+bigint
+string=>""
+boolean=>true or false
+null=>satndalone value
+undefined=>not assign
+
+object
+*/
+console.log(typeof 78);
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
